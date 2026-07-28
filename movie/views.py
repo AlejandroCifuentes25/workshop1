54e21d3c6ef93d8bb100e8121d42497089f5591c
Alejandro Cifuentes <acifuenta1@eafit.edu.co>
2026-07-28 10:40:56 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {'name': 'Alejandro Cifuentes'})
+    return render(request, 'home.html', {'name': 'Alejandro Cifuentes Arroyave'})
 
 def about(request):
     return HttpResponse( '<h1>Welcome to about page</h1>')
